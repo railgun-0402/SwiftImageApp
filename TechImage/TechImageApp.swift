98fdc2d09328a97ba10d56c1f81a6255611df966
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-09-22 00:25:08 +0900

    Initial Commit

---
+++ TechImage/TechImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechImageApp.swift
+//  TechImage
+//
+//  Created by 菅原大輝 on 2024/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TechImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TechImageTests/TechImageTests.swift
@@ -0,0 +1,17 @@
+//
+//  TechImageTests.swift
+//  TechImageTests
+//
+//  Created by 菅原大輝 on 2024/09/22.
+//
+
+import Testing
+@testable import TechImage
+
+struct TechImageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
