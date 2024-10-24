929306ede98fce32e5e7359da83e3650a0f1060e
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-10-25 01:08:34 +0900

    music

--- TechImage/ImageScroll.swift
+++ TechImage/ImageScroll.swift
@@ -15,11 +15,15 @@ struct KindStructImage {
 struct ImageScroll: View {
     @EnvironmentObject private var userData: UserData
     
+    // 音声データクラス作成
+    @ObservedObject private var speech: SpeechData = SpeechData()
+    
     // 編集モード(編集:active 完了：inactive)
     @State private var isEditMode: EditMode = .inactive
-    
     // 画像処理
     @State private var kindImage: Int = KindStructImage.regular
+    // 音声認識フラグ
+    @State private var isSpeech: Bool = false
     
     // お気に入り判定
     var onlyFavorite: Bool
@@ -36,8 +40,6 @@ struct ImageScroll: View {
                             if  (self.onlyFavorite == true &&
                                  item.isFavorite) ||
                                     self.onlyFavorite == false {
-                                
-                                // ==========ここから編集します==========
                                 
                                 // グレースケール
                                 if self.kindImage == KindStructImage.grayscale {
@@ -61,12 +63,13 @@ struct ImageScroll: View {
                                     Image(uiImage: UIImage.init(contentsOfFile: item.path)!)
                                         .resizable()
                                 }
-                                
-                                // ==========ここまで編集します==========
                             }
                         }
-                        // ここに追加します
                         .aspectRatio(contentMode: .fit)
+                        
+                        // 回転のアニメーション
+                        .rotationEffect(.degrees(self.speech.isRotation ? 360 : 0))
+                        .animation(self.speech.isRotation ? self.animation : .none)
                     }
                 }
                 
@@ -99,6 +102,32 @@ struct ImageScroll: View {
                         Image(systemName: "photo.on.rectangle")
                             .foregroundColor(Color.yellow)
                     }
+                    
+                    Spacer()
+                    
+                    // 音声認識ボタン
+                    Button(action: {
+                        
+                        if self.isSpeech {
+                            self.speech.stop()
+                            
+                        } else {
+                            self.speech.start()
+                        }
+                        self.isSpeech.toggle()
+                    }) {
+                        
+                        // マイク画像
+                        if self.isSpeech {
+                            Image(systemName: "mic")
+                                .foregroundColor(Color.red)
+                        }
+                        else {
+                            Image(systemName: "mic")
+                                .foregroundColor(Color.green)
+                        }
+                    }
+                    
                 }
                 // アイコンとツールバーの間に余白
                 .padding()
@@ -114,8 +143,32 @@ struct ImageScroll: View {
         .edgesIgnoringSafeArea(.bottom)
         .navigationBarItems(trailing: EditButton())
         .environment(\.editMode, self.$isEditMode)
+        .onAppear(perform: speechAuthorization)
+        .onDisappear(perform: speechStop)
+    }
+    
+    // 詳細なアニメーション設定
+    var animation: Animation {
+        
+        Animation.interpolatingSpring(mass: 1,
+                                      stiffness: 2,
+                                      damping: 0.8,
+                                      initialVelocity: 2)
+        .speed(2)
+    }
+
+    // 音声認識を停止します
+    func speechStop() {
+        self.speech.stop()
+    }
+
+    // 音声認識の許可ダイアログを表示します
+    func speechAuthorization() {
+        self.speech.authorization()
     }
 }
+
+
 
 
 #Preview {
